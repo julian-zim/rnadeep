f41313d44ead9be2685c5bd91260b9bd1f5563d2
Julian Zimmermann <julian.zimmermann.tue@gmail.com>
2024-01-11 18:46:39 +0100

    more run scripts

--- examples/run.sh
+++ examples/run.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/bash
-#SBATCH -J rnadeep
-#SBATCH --partition=zen3_0512_a100x2
-#SBATCH --qos zen3_0512_a100x2
-#SBATCH --gres=gpu:2
-#SBATCH --mail-user=a12144285@unet.univie.ac.at
-#SBATCH --mail-type=END,FAIL
-#SBATCH --output=/home/fs71475/julianz123/rnadeep/examples/slurm_out/gcn_slurm.%A.out
-#SBATCH --error=/home/fs71475/julianz123/rnadeep/examples/gcn_slurm.%A.error
 
 source /home/julian-zim/Programs/anaconda3/etc/profile.d/conda.sh
 conda activate spb
---
+++ rnaconv/data/full/run-vsc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+spack load miniconda3@4.12.0
+source /home/fs71475/julianz123/.bashrc
+conda activate /home/fs71475/julianz123/.conda/envs/jzspb
+
+python ../../alignment_generator.py 1 ./rfam/ ./sissi/ > ./output.txt
