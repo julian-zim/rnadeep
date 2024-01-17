8097c184a779281e149dfd037127135d38bad5e8
Julian <julian.zimmermann.tue@gmail.com>
2024-01-17 18:25:38 +0100

    tmep

---
+++ examples/run_seq.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+source /home/julian-zim/Programs/anaconda3/etc/profile.d/conda.sh
+conda activate spb
+
+python train.py --train-data-file ./data/uniform_len25-30_n10000.fa-train \
+                --validation-data-file ./data/uniform_len25-30_n2000.fa-test \
+                --data-tag l30 --smodel 3 --batch-size 50 --epochs 5
