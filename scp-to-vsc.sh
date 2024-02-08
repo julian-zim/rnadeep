a95556afdf7dfcb37389911a4489231c6b798e40
Julian Zimmermann <julian.zimmermann.tue@gmail.com>
2024-02-08 13:49:20 +0100

    added folder for slurm out

--- scp-to-vsc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#scp README.md install-vsc.sh setup.py julianz123@vsc5.vsc.ac.at:/home/fs71475/julianz123/workspace/rnadeep/
-#scp -r rnadeep julianz123@vsc5.vsc.ac.at:/home/fs71475/julianz123/workspace/rnadeep/
-#scp rnaconv/run-vsc.sh rnaconv/sissi099 rnaconv/alignment_generator.py rnaconv/rfam_converter.py julianz123@vsc5.vsc.ac.at:/home/fs71475/julianz123/workspace/rnadeep/rnaconv/
-#scp -r rnaconv/data julianz123@vsc5.vsc.ac.at:/home/fs71475/julianz123/workspace/rnadeep/rnaconv/
-#scp examples/train_ali.py examples/predict_ali.py julianz123@vsc5.vsc.ac.at:/home/fs71475/julianz123/workspace/rnadeep/examples/
-scp examples/slurm/* julianz123@vsc5.vsc.ac.at:/home/fs71475/julianz123/workspace/rnadeep/examples/slurm/
