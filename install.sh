201fbc4b7780f96efec148abfbf2daef38d8a4e3
Julian <julian.zimmermann.tue@gmail.com>
2024-02-25 16:13:06 +0100

    changed dataset creator

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-source /home/julian-zim/Programs/anaconda3/etc/profile.d/conda.sh
-conda activate rnadeep
-
-pip install .
-
--- rnaconv/test-dataset-creator.py
+++ rnaconv/test-dataset-creator.py
@@ -2,16 +2,14 @@ import os
 import subprocess
 import shutil
 import numpy as np
-import alignment_generator
+import family_generator
 
 
 def get_paths(rfam_path):
 	tree_dirpath = os.path.join(rfam_path, 'seed_trees', 'rescaled')
-	neigh_dirpath = os.path.join(rfam_path, 'seed_neighbourhoods', 'nei')
 	sfreq_dirpath = os.path.join(rfam_path, 'seed_frequencies', 'single')
 	dfreq_dirpath = os.path.join(rfam_path, 'seed_frequencies', 'doublet')
-	ali_dirpath = os.path.join(rfam_path, 'seed_alignments')
-	return [tree_dirpath, neigh_dirpath, sfreq_dirpath, dfreq_dirpath, ali_dirpath]
+	return [tree_dirpath, sfreq_dirpath, dfreq_dirpath]
 
 
 def script(batchsize, epoch, seqlength, alicount):
@@ -27,16 +25,16 @@ def script(batchsize, epoch, seqlength, alicount):
 				'eval \"$(conda shell.bash hook)\"\n'
 				'conda activate rnadeep\n'
 				'\n'
-				'python ../train_ali.py --ali-dir ../../rnaconv/data/generated/alignment/' + filename_lcc + '/alignments/ \\\n'
-				'\t   --dbn-dir ../../rnaconv/data/rfam/' + filename_lcc + '/seed_neighbourhoods/dbn/ \\\n'
+				'python ../train_ali.py --ali-dir ../../rnaconv/data/generated/family/' + filename_lcc + '/alignments/ \\\n'
+				'\t   --dbn-dir ../../rnaconv/data/generated/family/' + filename_lcc + '/seed_neighbourhoods/dbn/ \\\n'
 				'\t   --model-log-dir ../models \\\n'
 				'\t   --data-tag sm3-' + tag + '-sissi --smodel 3 --batch-size ' + str(batchsize) + ' --epochs ' + str(epoch) + '\n')
 
 	with open(os.path.join('../examples/slurm', filename + '.slrm'), 'w') as scriptfile:
 		scriptfile.write('#!/bin/sh\n'
 						 '#SBATCH -J ' + jobname + '\n'
-						 '#SBATCH --partition=zen2_0256_a40x2\n'
-						 '#SBATCH --qos zen2_0256_a40x2\n'
+						 '#SBATCH --partition=zen3_0512_a100x2\n'
+						 '#SBATCH --qos zen3_0512_a100x2\n'
 						 '#SBATCH --gres=gpu:2\n'
 						 '#SBATCH --mail-user=a12144285@unet.univie.ac.at\n'
 						 '#SBATCH --mail-type=BEGIN,END,FAIL\n'
@@ -84,8 +82,8 @@ def copy(from_path, to_path, filenames):
 				os.path.exists(os.path.join(from_path, neigh_dbn_path, filename + '.dbn')),
 				os.path.exists(os.path.join(from_path, neigh_ct_path, filename + '.ct')),
 				os.path.exists(os.path.join(from_path, neigh_nei_path, filename + '.nei')),
-				os.path.exists(os.path.join(from_path, single_freq_path, filename + '.freq')),
-				os.path.exists(os.path.join(from_path, doublet_freq_path, filename + '.freq'))]
+				os.path.exists(os.path.join(from_path, single_freq_path, filename + '.sfreq')),
+				os.path.exists(os.path.join(from_path, doublet_freq_path, filename + '.dfreq'))]
 		if all(condition):
 			shutil.copy(os.path.join(from_path, tree_orig_path, filename + '.seed_tree'),
 						os.path.join(to_path, tree_orig_path, filename + '.seed_tree'))
@@ -103,50 +101,42 @@ def copy(from_path, to_path, filenames):
 						os.path.join(to_path, neigh_ct_path, filename + '.ct'))
 			shutil.copy(os.path.join(from_path, neigh_nei_path, filename + '.nei'),
 						os.path.join(to_path, neigh_nei_path, filename + '.nei'))
-			shutil.copy(os.path.join(from_path, single_freq_path, filename + '.freq'),
-						os.path.join(to_path, single_freq_path, filename + '.freq'))
-			shutil.copy(os.path.join(from_path, doublet_freq_path, filename + '.freq'),
-						os.path.join(to_path, doublet_freq_path, filename + '.freq'))
+			shutil.copy(os.path.join(from_path, single_freq_path, filename + '.sfreq'),
+						os.path.join(to_path, single_freq_path, filename + '.sfreq'))
+			shutil.copy(os.path.join(from_path, doublet_freq_path, filename + '.dfreq'),
+						os.path.join(to_path, doublet_freq_path, filename + '.dfreq'))
 		else:
 			print('Skipping \'' + filename + '\' as it is missing data. (Raw output: ' + str(condition) + ')')
 
 
 def create(alicount, seqlength, batch_sizes, epochs):
 	rfam_path = 'data/rfam'
-	generated_path = 'data/generated/alignment'
+	generated_path = 'data/generated/family'
 	foldername = 'AT-' + 'L' + str(seqlength) + '-ac' + str(alicount)
 	subprocess.run(['rm', '-rf', str(os.path.join(rfam_path, foldername))])
 	os.makedirs(os.path.join(rfam_path, foldername), exist_ok=False)
 	subprocess.run(['rm', '-rf', str(os.path.join(generated_path, foldername))])
 	os.makedirs(os.path.join(generated_path, foldername), exist_ok=False)
 
-	filenames = os.listdir(os.path.join(rfam_path, 'full/seed_alignments'))
-	alilist = list()
-	while len(alilist) < 1:
-		id = np.random.choice(range(len(filenames)), 1)[0]
-		with open(os.path.join(os.path.join(rfam_path, 'full/seed_alignments'), filenames[id]), 'r') as candidate:
-			line = candidate.readline()
-			candidate_seqlength = len(candidate.readline().split()[1])
+	filenames = os.listdir(os.path.join(rfam_path, 'full/seed_trees/rescaled'))
+	filename = [filenames[np.random.randint(len(filenames))].split('.')[0]]
 
-		if candidate_seqlength == seqlength:
-			alilist.append(filenames[id].split('.')[0])
-		del filenames[id]
-
-	copy(os.path.join(rfam_path, 'full'), os.path.join(rfam_path, foldername), alilist)
-	alignment_generator.generate_alignment_set('./sissi099',
-											   alicount,
-											   *get_paths(os.path.join(rfam_path, foldername)),
-											   os.path.join(generated_path, foldername))
+	copy(os.path.join(rfam_path, 'full'), os.path.join(rfam_path, foldername), filename)
+	family_generator.generate_family_set('./sissi099',
+										 alicount,
+										 seqlength,
+										 *get_paths(os.path.join(rfam_path, foldername)),
+										 os.path.join(generated_path, foldername))
 	for batch_size in batch_sizes:
 		for epoch in epochs:
 			script(batch_size, epoch, seqlength, alicount)
 
 
 def main():
-	alicounts = [100]
-	seqlengths = [200]  # [100, 200, 301, 401, 503, 604, 673]
-	batch_sizes = [5]  # [1, 2, 3, 4, 5]
-	epochs = [1, 2, 3, 4, 5]
+	alicounts = [5]
+	seqlengths = [285, 290, 295, 300]  # [100, 200, 301, 401, 503, 604, 673]
+	batch_sizes = [4]  # [1, 2, 3, 4, 5]
+	epochs = [1]
 
 	for alicount in alicounts:
 		for seqlength in seqlengths:
